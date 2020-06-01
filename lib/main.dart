c95c4c36dc3b0eb0a4a48bcb3dec78ad0f688a51
Muhammad Fakhri <muhammad_fakhri@apps.ipb.ac.id>
2020-06-01 07:11:44 +0700

    Update codes for Route section

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,31 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
+// You can use a relative import, i.e. `import 'category_route.dart;'` or
+// a package import, as shown below.
+// More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
+import 'category_route.dart';
+
+/// The function that is called when main.dart is run.
 void main() {
-  runApp(MyApp());
+  runApp(UnitConverterApp());
 }
 
-class MyApp extends StatelessWidget {
+/// This widget is the root of our application.
+///
+/// The first screen we see is a list [Categories], each of which
+/// has a list of [Unit]s.
+class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Learn Flutter Application")),
-        ),
-        body: Center(
-            child: Container(
-          color: Colors.lightBlue,
-          margin: EdgeInsets.all(16),
-          padding: EdgeInsets.fromLTRB(0, 16, 16, 16),
-          child: Container(
-            color: Colors.blueAccent,
-          ),
-        )),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: CategoryRoute(),
     );
   }
 }
